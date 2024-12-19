9ca24de4efaa74c02cec9cdf0be79a66ba0481d0
Stephen Meycelino Fransisco <fstephenmeycelino@gmail.com>
2024-12-20 00:32:54 +0700

    feat: Get, GetById, Update, Delete Product

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/api", (req, res) => {
   res.send("API is running");
 });
@@ -16,6 +18,99 @@ app.get("/api", (req, res) => {
 app.get("/products", async (req, res) => {
   const products = await prisma.product.findMany();
   res.send(products);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const product = await prisma.product.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!product) {
+    res.status(400).send("Product not found");
+  }
+
+  res.send(product);
+});
+
+app.post("/products", async (req, res) => {
+  const { name, description, image, price } = req.body;
+
+  const product = await prisma.product.create({
+    data: {
+      name,
+      description,
+      image,
+      price,
+    },
+  });
+
+  res.send({
+    data: product,
+    message: "Product created successfully",
+  });
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await prisma.product.delete({
+    where: {
+      id,
+    },
+  });
+
+  res.send("Product deleted successfully");
+});
+
+app.put("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, description, image, price } = req.body;
+
+  if (!name || !description || !image || !price) {
+    return res.status(400).send("All fields are required");
+  }
+
+  const product = await prisma.product.update({
+    where: {
+      id,
+    },
+    data: {
+      description,
+      image,
+      name,
+      price,
+    },
+  });
+
+  res.send({
+    data: product,
+    message: "Product updated successfully",
+  });
+});
+
+app.patch("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, description, image, price } = req.body;
+
+  const product = await prisma.product.update({
+    where: {
+      id,
+    },
+    data: {
+      description,
+      image,
+      name,
+      price,
+    },
+  });
+
+  res.send({
+    data: product,
+    message: "Product updated successfully",
+  });
 });
 
 app.listen(PORT, () => {
